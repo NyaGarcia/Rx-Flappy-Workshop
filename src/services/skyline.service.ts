50cac4b870fafce157f2ae1cc7a9d9ce4a0798a1
Nya Garcia <nyablk97@gmail.com>
2019-11-03 16:35:22 +0100

    feat(skyline.service): add typings

--- src/services/skyline.service.ts
+++ src/services/skyline.service.ts
@@ -4,25 +4,25 @@ import { Skyline } from '../models/skyline.model';
 export class SkylineService {
   constructor(private frameUpdate$: Observable<number>, private skylines: PIXI.Container) {}
 
-  public getSkylines() {
+  public getSkylines(): PIXI.Container {
     return this.skylines;
   }
 
-  public getLastSkylineObject() {
+  public getLastSkylineObject(): PIXI.DisplayObject {
     const { children } = this.skylines;
     return children[children.length - 1];
   }
 
-  public addSkyline(skyline: Skyline) {
+  public addSkyline(skyline: Skyline): void {
     this.skylines.addChild(skyline.getSprite());
     this.subscribe(skyline);
   }
 
-  public deleteSkyline(skyline: PIXI.DisplayObject) {
+  public deleteSkyline(skyline: PIXI.DisplayObject): void {
     this.skylines.removeChild(skyline);
   }
 
-  private subscribe(skyline: Skyline) {
+  private subscribe(skyline: Skyline): void {
     this.frameUpdate$.subscribe(delta => skyline.updatePosition(delta));
   }
 }
