aa0eef16d48f1901d9aaeb3c0d94458a0f30b6ca
Nya Garcia <nyablk97@gmail.com>
2019-11-03 16:20:15 +0100

    feat(pipe.service): implement pipe sprite creation

--- src/services/pipe.service.ts
+++ src/services/pipe.service.ts
@@ -1,5 +1,24 @@
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+
+import { Pipe } from '../models/pipe.model';
 
 export class PipeService {
-  constructor(private frameUpdate$: Observable<number>) {}
+  constructor(private frameUpdate$: Observable<number>, private stage: PIXI.Container) {}
+
+  public addPipe(pipe: Pipe) {
+    this.stage.addChild(pipe.getSprite());
+    this.subscribe(pipe);
+  }
+
+  public deleteOldPipes() {
+    this.stage.children
+      .filter(Boolean)
+      .filter(({ type }) => type === 'pipe')
+      .filter(({ position }) => position.x < 0)
+      .forEach(pipe => this.stage.removeChild(pipe));
+  }
+
+  private subscribe(pipe: Pipe) {
+    this.frameUpdate$.subscribe(delta => pipe.updatePosition(delta));
+  }
 }
