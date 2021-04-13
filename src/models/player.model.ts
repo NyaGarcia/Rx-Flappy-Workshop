7621a4cc93767ed7cb67b22296c0427a13078547
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2021-04-13 19:36:17 +0200

    feat(player.model): remove rxjs logic

--- src/models/player.model.ts
+++ src/models/player.model.ts
@@ -1,47 +1,34 @@
 import * as PIXI from 'pixi.js';
 
 import { CANVAS_SIZE, SPRITE_URLS } from '../constants/game_config.constants';
-import { delay, filter, tap } from 'rxjs/operators';
 
-import { Observable } from 'rxjs';
+import { Sprite } from 'pixi.js';
 
 export class Player {
-  public sprite: any;
+  private _sprite: Sprite;
+  private ySpeed = 0;
 
-  constructor(public stage: PIXI.Container, private flap$: Observable<KeyboardEvent>) {
-    this.createGameObject();
-    this.suscribeObservables();
+  constructor() {
+    this._sprite = Sprite.from(SPRITE_URLS.PLAYER.INITIAL);
+    this.ySpeed = 0;
+    this.sprite.anchor.set(0.5);
+    this.sprite.position.set(250, CANVAS_SIZE.HEIGHT / 2);
+    this.sprite.scale.set(5);
+  }
+
+  public get sprite() {
+    return this._sprite;
   }
 
   public killKiwi() {
     this.sprite.rotation = 180;
   }
 
-  private createGameObject() {
-    this.sprite = PIXI.Sprite.from(SPRITE_URLS.PLAYER.INITIAL);
-    this.sprite.anchor.set(0.5);
-    this.sprite.position.set(250, CANVAS_SIZE.HEIGHT / 2);
-    this.sprite.scale.set(5);
-
-    this.stage.addChild(this.sprite);
-  }
-
-  private suscribeObservables() {
-    this.flap$
-      .pipe(
-        filter(({ keyCode }) => keyCode === 32 || keyCode === 38),
-        tap(() => this.flap()),
-        delay(150),
-        tap(() => this.changeAnimation(SPRITE_URLS.PLAYER.INITIAL)),
-      )
-      .subscribe();
-  }
-
-  private flap() {
+  public flap() {
     this.changeAnimation(SPRITE_URLS.PLAYER.FLAPPING);
   }
 
-  private changeAnimation(url: string) {
+  public changeAnimation(url: string) {
     const texture = PIXI.Texture.from(url);
     this.sprite.texture = texture;
   }
