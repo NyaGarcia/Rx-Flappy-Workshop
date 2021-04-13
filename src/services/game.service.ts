0ef81630d94c3fb0f466fe714e693b9eff2bb3e6
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2021-04-13 20:00:09 +0200

    feat(game.service): add TODO 1 and 2

--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -1,1 +1,9 @@
-export class GameService {}
+import { Observable } from 'rxjs';
+
+export class GameService {
+  /* TODO 1 (hint: use fromEvent) */
+  private pressedKey$: Observable<KeyboardEvent>;
+
+  /* TODO 2 (hint: create an Observable from pressedKey$ and use the filter operator) */
+  public onFlap$: Observable<KeyboardEvent>;
+}
