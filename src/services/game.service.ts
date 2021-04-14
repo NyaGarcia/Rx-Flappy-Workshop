77d6c7645eaaa7cf90a8ab7b800d211bf7bb048c
NyaGarcia <nyablk97@gmail.com>
2021-04-14 18:00:03 +0200

    feat(game.service): add TODO 1 solution

--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -1,6 +1,6 @@
-import { Observable, Subject, fromEvent, interval, timer } from 'rxjs';
+import { KEYS, PHYSICS } from '../constants/game-config.constants';
+import { Subject, fromEvent, interval, timer } from 'rxjs';
 
-import { KEYS } from '../constants/game-config.constants';
 import { filter } from 'rxjs/operators';
 
 export class GameService {
@@ -14,7 +14,9 @@ export class GameService {
 
   public skylineUpdate$ = interval(1000);
 
-  /* TODO 1 (hint: create an observable that waits PHYSICS.PIPE_GENERATION_FIRST_WAIT seconds,
-     then starts emitting values every PHYSICS.PIPE_GENERATION_INTERVAL seconds) */
-  public createObstacle$: Observable<number>;
+  // TODO 1 Solution
+  public createObstacle$ = timer(
+    PHYSICS.PIPE_GENERATION_FIRST_WAIT,
+    PHYSICS.PIPE_GENERATION_INTERVAL,
+  );
 }
