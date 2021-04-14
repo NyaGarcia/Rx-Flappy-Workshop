e749c24ff3705d4c4c8d66ef24b733791b04c0ee
NyaGarcia <nyablk97@gmail.com>
2021-04-14 18:43:12 +0200

    feat(game.service): add TODO 1

--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -1,5 +1,5 @@
 import { KEYS, PHYSICS } from '../constants/game-config.constants';
-import { Subject, fromEvent, interval, timer } from 'rxjs';
+import { Observable, Subject, fromEvent, interval, timer } from 'rxjs';
 
 import { filter } from 'rxjs/operators';
 
@@ -14,9 +14,11 @@ export class GameService {
 
   public skylineUpdate$ = interval(1000);
 
-  // TODO 1 Solution
   public createObstacle$ = timer(
     PHYSICS.PIPE_GENERATION_FIRST_WAIT,
     PHYSICS.PIPE_GENERATION_INTERVAL,
   );
+
+  // TODO 1 (hint: use scan + timer to accumulate the score)
+  public score$: Observable<number>;
 }
