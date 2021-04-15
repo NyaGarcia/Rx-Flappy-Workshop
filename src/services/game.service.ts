a29387a2227f29133ee3168d1e76269a84b4d845
NyaGarcia <nyablk97@gmail.com>
2021-04-15 12:31:09 +0200

    feat(game.service): add solutions

--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -1,39 +1,41 @@
 import { KEYS, PHYSICS } from '../constants/game-config.constants';
 import { Observable, Subject, fromEvent, interval, timer } from 'rxjs';
-import { filter, scan } from 'rxjs/operators';
+import { filter, scan, takeUntil } from 'rxjs/operators';
 
 export class GameService {
-  // TODO 1 (hint: create Subject)
-  private stopGame$: Subject<void>;
+  // TODO 1 Solution
+  private stopGame$ = new Subject<void>();
 
   private pressedKey$ = fromEvent<KeyboardEvent>(document, 'keydown');
 
-  // TODO 3 (hint: use takeUntil)
-  public onFrameUpdate$ = new Subject<number>() as Subject<number>;
+  // TODO 3 Solution
+  public onFrameUpdate$ = new Subject<number>().pipe(takeUntil(this.stopGame$)) as Subject<number>;
 
-  // TODO 3 (hint: use takeUntil)
+  // TODO 3 Solution
   public onFlap$ = this.pressedKey$.pipe(
     filter(({ code }) => code === KEYS.SPACE || code === KEYS.UP),
+    takeUntil(this.stopGame$),
   );
 
-  // TODO 3 (hint: use takeUntil)
-  public skylineUpdate$ = interval(1000);
+  // TODO 3 Solution
+  public skylineUpdate$ = interval(1000).pipe(takeUntil(this.stopGame$));
 
-  // TODO 3 (hint: use takeUntil)
+  // TODO 3 Solution
   public createObstacle$ = timer(
     PHYSICS.PIPE_GENERATION_FIRST_WAIT,
     PHYSICS.PIPE_GENERATION_INTERVAL,
-  );
+  ).pipe(takeUntil(this.stopGame$));
 
-  // TODO 3 (hint: use takeUntil)
+  // TODO 3 Solution
   public score$ = timer(PHYSICS.SCORE_FIRST_WAIT, PHYSICS.PIPE_GENERATION_INTERVAL).pipe(
     scan(score => score + 1, 0),
+    takeUntil(this.stopGame$),
   );
 
   public stopGame() {
-    // TODO 2 (hint: make stopGame$ emit)
+    this.stopGame$.next();
   }
 
-  // TODO 6 (hint: create an Observable that emits ONLY when space is pressed)
-  public restart$: Observable<KeyboardEvent>;
+  // TODO 6 Solution
+  public restart$ = this.pressedKey$.pipe(filter(({ code }) => code === KEYS.SPACE));
 }
