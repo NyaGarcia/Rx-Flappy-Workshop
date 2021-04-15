85fcebb3b6464f36f5fbfad383c56b42201a58db
NyaGarcia <nyablk97@gmail.com>
2021-04-15 12:38:59 +0200

    feat(game.service): add TODO 1

--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -3,30 +3,24 @@ import { Observable, Subject, fromEvent, interval, timer } from 'rxjs';
 import { filter, scan, takeUntil } from 'rxjs/operators';
 
 export class GameService {
-  // TODO 1 Solution
   private stopGame$ = new Subject<void>();
 
   private pressedKey$ = fromEvent<KeyboardEvent>(document, 'keydown');
 
-  // TODO 3 Solution
   public onFrameUpdate$ = new Subject<number>().pipe(takeUntil(this.stopGame$)) as Subject<number>;
 
-  // TODO 3 Solution
   public onFlap$ = this.pressedKey$.pipe(
     filter(({ code }) => code === KEYS.SPACE || code === KEYS.UP),
     takeUntil(this.stopGame$),
   );
 
-  // TODO 3 Solution
   public skylineUpdate$ = interval(1000).pipe(takeUntil(this.stopGame$));
 
-  // TODO 3 Solution
   public createObstacle$ = timer(
     PHYSICS.PIPE_GENERATION_FIRST_WAIT,
     PHYSICS.PIPE_GENERATION_INTERVAL,
   ).pipe(takeUntil(this.stopGame$));
 
-  // TODO 3 Solution
   public score$ = timer(PHYSICS.SCORE_FIRST_WAIT, PHYSICS.PIPE_GENERATION_INTERVAL).pipe(
     scan(score => score + 1, 0),
     takeUntil(this.stopGame$),
@@ -36,6 +30,8 @@ export class GameService {
     this.stopGame$.next();
   }
 
-  // TODO 6 Solution
   public restart$ = this.pressedKey$.pipe(filter(({ code }) => code === KEYS.SPACE));
+
+  // TODO 1 (hint: create an Observable that emits if keys are pressed more than 6 times in 1s)
+  public easterEgg$: Observable<KeyboardEvent[]>;
 }
